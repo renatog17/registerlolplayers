7fc8777562c1a289d80ffa4c9cc509ad7fb6da3e
Renato Oliveira <renatog.oliveira17@gmail.com>
2023-01-04 20:43:42 -0300

    creating the project

---
+++ src/main/java/br/com/portfolio/registerlolplayers/RegisterlolplayersApplication.java
@@ -0,0 +1,13 @@
+package br.com.portfolio.registerlolplayers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegisterlolplayersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegisterlolplayersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/portfolio/registerlolplayers/RegisterlolplayersApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.portfolio.registerlolplayers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegisterlolplayersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
